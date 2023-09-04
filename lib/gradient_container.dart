4fcc847da577e69b13aa7d9b48cba6df4ad44d16
Yojith Kaustabh <yojith212@gmail.com>
2023-09-04 15:33:13 +0530

    day 1, made it on my own

---
+++ lib/gradient_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/intro_pic.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepOrange,
+        color2 = Colors.purpleAccent;
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(child: InPic()),
+    );
+  }
+}
---
+++ lib/intro_pic.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class InPic extends StatefulWidget {
+  const InPic({super.key});
+  @override
+  State<InPic> createState() {
+    return _Shower();
+  }
+}
+
+class _Shower extends State<InPic> {
+  void rollDice() {
+    setState(() {
+      const Text("Real");
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Text(
+          "The Ultimate Quiz",
+          style: TextStyle(color: Colors.white, fontSize: 40),
+        ),
+        Image.asset(
+          'assets/images/tmgyho.png',
+          width: 200,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.only(
+              top: 20,
+              bottom: 20,
+            ),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Are You Ready?'),
+        )
+      ],
+    );
+  }
+}
